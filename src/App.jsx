6a5687f122579738ad42d4ef20adb44c9fe3f4b7
UltraPotatoCoder <guicarv99@gmail.com>
2024-09-10 11:04:53 +0100

    added useEffect simple functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // The code that we want to run
+    console.log('The count is', count);
+    // Optional return funtion (cleanup function)
+    return () => {
+      console.log('I am being cleaned up!');
+    };
+  }, [count]); // The dependency array
 
   return (
     <>
@@ -21,9 +30,6 @@ function App() {
         <button onClick={() => setCount(count => count + 1)}>+</button>
         <h2>{count}</h2>
         <button onClick={() => setCount(count => count - 1)}>-</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
       <p className='read-the-docs'>
         Click on the Vite and React logos to learn more
